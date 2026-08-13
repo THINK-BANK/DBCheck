694a49de0a9ce55b2963baaec82b5a712915dead
973721777 <973721777@qq.com>
2026-08-14 00:34:03 +0800

    update version

--- build/package_windows.py
+++ build/package_windows.py
@@ -3,7 +3,7 @@
 Usage: python package_windows.py <distpath> <version>
 
   distpath  - path to 'dist' directory (contains DBCheck-Windows folder)
-  version   - version string, e.g. v26.8.13.0
+  version   - version string, e.g. v26.8.14.0
 """
 import os
 import shutil
